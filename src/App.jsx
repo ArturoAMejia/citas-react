5b0b5c1fa0ea7bf76a3c1bac40bbba86c4e2e72c
Arturo Mejia <armejia02@gmail.com>
2022-02-13 17:23:16 -0600

    dando estilos al formulario

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Formulario/>
-      <ListadoPacientes/>
-    </>
+    <div className="container mx-auto mt-2">
+      <Header />
+      <div className="mt-12 md:flex">
+        <Formulario />
+        <ListadoPacientes />
+      </div>
+    </div>
   );
 }
 
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,96 @@
 const Formulario = () => {
   return (
-    <div className="">
-        <h2>Formulario</h2>
-    </div>
-  )
-}
+    <div className="md:w-1/2 lg:w-2/5 ">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade Pacientes y {""}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
 
-export default Formulario
+      <form action="" className="bg-white shadow-md rounded-lg py-10 px-5">
+        <div className="mb-5">
+          <label
+            htmlFor="mascota"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Mascota
+          </label>
+          <input
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la Mascota "
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="propetario"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Propetario
+          </label>
+          <input
+            id="propetario"
+            type="text"
+            placeholder="Nombre del propetario "
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email contacto propetario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="alta"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Alta
+          </label>
+          <input
+            id="alta"
+            type="date"
+            placeholder="Alta de la mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="sintomas"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Sintomas
+          </label>
+          <textarea
+            name=""
+            id="sintomas"
+            placeholder="Describe los Sintomas de la mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          ></textarea>
+        </div>
+
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold rounded-md hover:bg-indigo-700"
+          value={"Agregar Paciente"}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
